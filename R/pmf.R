3a583cbc539e8bec90072df5b68c5a13b8cb979f
Wojciech Francuzik <wolass@gmail.com>
2017-12-18 10:33:50 +0100

    Docker, NAMESPACE update and FIGURES pub ready.

--- R/pmf.R
+++ R/pmf.R
@@ -1,3 +1,12 @@
+#' This function is going to generate the + insted of a TRUE value
+#'
+#' Use it for table generation where T or F values need to be nicely presented
+#'
+#' @param x Include the vector to be tranformed
+#' @return a vector of + and - values
+#' @export
+
+
 pmf <- function(x){
   ifelse(x==T,"+","-")
 }
